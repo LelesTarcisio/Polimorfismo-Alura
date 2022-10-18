2ad788e82ef3418a27b5cb5a6075dafaff17e120
Tarcísio Leles de Paula <98960968+LelesTarcisio@users.noreply.github.com>
2022-10-17 22:52:47 -0300

    Interfaces

--- src/bytebank-herdado/src/Gerente.java
+++ src/bytebank-herdado/src/Gerente.java
@@ -1,9 +1,27 @@
-public class Gerente extends FuncionarioAutenticavel {
-    
+//Gerente é um funcionario e herda a classe Funcionario
+//Gerente também assina o contrato Autenticavel
 
-	public double getBonificacao() {
-		return super.getSalario();
-	}
-	
-	
+public class Gerente extends Funcionario implements Autenticavel {
+
+    private AutenticacaoUtil autenticador;
+
+    public Gerente() {
+        this.autenticador = new AutenticacaoUtil(); // não entendi o motivo de criar um construtor
+    }
+
+    public double getBonificacao() {
+        return super.getSalario();
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.autenticador.setSenha(senha); // ????????????????????
+
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        return this.autenticador.autentica(senha); // ??????????????????????????
+    }
+
 }
